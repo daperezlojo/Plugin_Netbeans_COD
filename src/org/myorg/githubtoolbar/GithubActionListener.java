a50b5bb76fcec3c3a81db5036079dd6bf513e705
daperezlojo <32535705+daperezlojo@users.noreply.github.com>
2018-06-16 00:23:50 +0200

    Creacion metodo para añadir repositorio y comentarios

--- src/org/myorg/githubtoolbar/GithubActionListener.java
+++ src/org/myorg/githubtoolbar/GithubActionListener.java
@@ -26,10 +26,30 @@ import org.openide.util.NbBundle.Messages;
 )
 @ActionReference(path = "Toolbars/File", position = 0)
 @Messages("CTL_GithubActionListener=GitHub")
+
+/**
+ * Clase en la que se creará el repositorio de Github.
+ */
 public final class GithubActionListener implements ActionListener {
 
+    /**
+     * Método que, tras hacer click en el botón (evento) pide un nombre para
+     * crear el repositorio en Github.
+     * @param e Evento de hacer click en el boton.
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+        // Se piden las credenciales para poder crear el repositorio:
+        String user = JOptionPane.showInputDialog("Nombre del usuario:");
+        String pass = JOptionPane.showInputDialog("Contraseña:");
+        String nombre = JOptionPane.showInputDialog("Nombre para el repositorio:");
+        try {
+            GitHub github = GitHub.connectUsingPassword(user, pass);
+            GHCreateRepositoryBuilder builder;
+            builder = github.createRepository(nombre);
+            builder.create();
+        } catch (IOException ex) {
+            System.out.println("Mensaje de error:" + ex);
+        }
     }
 }
